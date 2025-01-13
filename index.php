c9d1c84c7df8ca17e0cd9f6b3da552053adac3a3
Sastama <103018192+sastamaa@users.noreply.github.com>
2025-01-13 23:55:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,24 @@
 <?php
+$query = strtolower($_GET['query'] ?? '');
 
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-header('Content-Type: application/json');
-// TMDb API Key
-$api_key = "28797e7035babad606ddbc1642d2ec8b";
+// Load data files
+$movies = json_decode(file_get_contents('https://sastamaa.github.io/sa_addon/movies/list.json'), true);
+$series = json_decode(file_get_contents('https://sastamaa.github.io/sa_addon/series/list.json'), true);
 
-// List of movies (add only tmdb_id and streaming_url here)
-$movies = [
-    [
-        "tmdb_id" => "585083", 
-        "streaming_url" => "https://s1.hdvbua.pro/media1/content/stream/films/hotel_transylvania_transformania_2022_webrip_1080p_83600/hls/1080/index.m3u8"
-    ],
-    [
-        "tmdb_id" => "616747", 
-        "streaming_url" => "https://s1.hdvbua.pro/media1/content/stream/films/haunted_mansion_2023_webdl_1080p_91673/hls/1080/index.m3u8"
-    ]
-];
-
-$output = [
-    "type" => "list",
-    "headline" => "Movies",
-    "entries" => []
-];
-
-// Fetch movie details from TMDb
+// Merge and filter results
+$results = [];
 foreach ($movies as $movie) {
-    $tmdb_id = $movie['tmdb_id'];
-    $streaming_url = $movie['streaming_url'];
-
-    // Fetch movie data from TMDb
-    $response = file_get_contents("https://api.themoviedb.org/3/movie/{$tmdb_id}?api_key={$api_key}&language=en-US");
-    $details = json_decode($response, true);
-
-    // Check if the API call was successful
-    if (isset($details['title'])) {
-        $output['entries'][] = [
-            "title" => $details['title'],
-            "image" => "https://image.tmdb.org/t/p/w500" . $details['poster_path'],
-            "description" => $details['overview'],
-            "data" => $streaming_url
-        ];
+    if (strpos(strtolower($movie['title'] ?? ''), $query) !== false) {
+        $results[] = $movie;
+    }
+}
+foreach ($series as $show) {
+    if (strpos(strtolower($show['title'] ?? ''), $query) !== false) {
+        $results[] = $show;
     }
 }
 
-// Output final JSON
+// Output JSON
 header('Content-Type: application/json');
-echo json_encode($output, JSON_PRETTY_PRINT);
+echo json_encode($results);
 ?>
